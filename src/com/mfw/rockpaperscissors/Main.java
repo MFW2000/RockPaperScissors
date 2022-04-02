2ae40df405625ce412d13e88a9ed18894fa0302c
MFW2000 <marijnwelmerink@gmail.com>
2022-04-02 21:25:28 +0200

    Initial commit

---
+++ src/com/mfw/rockpaperscissors/Main.java
@@ -0,0 +1,48 @@
+package com.mfw.rockpaperscissors;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    private static final String[] gameChoices = {"r", "p", "s"};
+    private static final String[] menuChoices = {"exit", ""};
+
+    public static void main(String[] args) {
+        Random random = new Random();
+        String[] results = {"You win!", "You lose!", "It's a draw!"};
+        String input = "";
+
+        System.out.println("Welcome to Rock Paper Scissors.");
+
+        while (!input.equalsIgnoreCase("exit")) {
+            System.out.print("Rock, Paper, Scissors? - r, p, s: ");
+            readString(gameChoices);
+
+            int outcome = random.nextInt(results.length);
+            System.out.println(results[outcome]);
+            System.out.println();
+
+            System.out.print("Leave empty to play again or type 'exit' to quit: ");
+            input = readString(menuChoices);
+        }
+    }
+
+    private static String readString(String[] possibleChoices) {
+        Scanner scanner = new Scanner(System.in);
+        boolean isValid = false;
+        String result = "";
+
+        while (!isValid) {
+            result = scanner.nextLine();
+
+            if (!Arrays.asList(possibleChoices).contains(result.toLowerCase())) {
+                System.out.print("Invalid input, try again: ");
+            } else {
+                isValid = true;
+            }
+        }
+
+        return result;
+    }
+}
